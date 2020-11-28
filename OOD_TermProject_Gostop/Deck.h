d30205a59688523366964e9f7ae4cd344c702692
201702083 <73640793+201702083@users.noreply.github.com>
2020-11-28 19:07:34 +0900

    run()

--- OOD_TermProject_Gostop/Deck.h
+++ OOD_TermProject_Gostop/Deck.h
@@ -4,13 +4,15 @@
 #include <vector>
 #include <stack>
 #include "cardBuilder.h"
+#include "player.h"
 
 class Deck {
  public:
-  void prints();               // 손에 있는 n번째 패 내기
+  void prints();  
   void Shuffle();  // 카드를 딴 카드배열에 추가
   std::vector<Card*>* GetFloor();
   std::stack<Card*>* GetDeck();
+  void Run(Player turn, Player other1, Player other2, Card* n);
   Deck();
 
 
@@ -18,7 +20,6 @@ class Deck {
 
   std::stack<Card*>* deck;
   std::vector<Card*>* floor;
-  std::vector<Card*>* get;
 };
 
 #endif
